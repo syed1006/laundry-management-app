ea14aa3665ae303710c9ef22334511810d6d0a44
Syed Salman <syedsalman1006@gmail.com>
2023-02-14 18:56:44 +0530

    added css to login component

---
+++ client/src/components/CompanyText/CompanyText.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const CompanyText = ({path, text}) => {
+    return (
+        <section className="text-container">
+            <h1 className='heading-logo'><span>Laundry </span><span>Service</span></h1>
+            <h6 className='tag-line' >Doorstep Wash & Dryclean Service</h6>
+            <p>Don't Have An Account</p>
+            <Link to={path}>{text}</Link>
+        </section>
+    )
+}
+
+export default CompanyText;
